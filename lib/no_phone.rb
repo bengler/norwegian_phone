16331f5c860be25b31d09ba5a65b736ff5f02ceb
Katrina Owen <katrina.owen@gmail.com>
2011-04-27 17:25:27 +0200

    tweaking for readability

--- lib/no_phone.rb
+++ lib/no_phone.rb
@@ -1,21 +1,22 @@
 module NoPhone
 
-  def self.normalize(number)
-    number ||= ""
-    number = number.gsub(/[.\- \t\r\n\(\)]/, '')
-    number.gsub!(/^(\+)(.*)/) { |m| $1 + $2.gsub(/\+/, '') }
-    number.gsub!(/^00/, '+')
-    number.gsub!(/^\+47/, '')
-    number.gsub!(/^\+*0+/, '+')
-    number.gsub!(/^47/, '') if number.size > 8
-    number
-  end
+  class << self
+    def normalize(number)
+      number ||= ""
+      number = number.gsub(/[.\- \t\r\n\(\)]/, '')
+      number.gsub!(/^(\+)(.*)/) { |m| $1 + $2.gsub(/\+/, '') }
+      number.gsub!(/^00/, '+')
+      number.gsub!(/^\+47/, '')
+      number.gsub!(/^\+*0+/, '+')
+      number.gsub!(/^47/, '') if number.size > 8
+      number
+    end
 
-  # Normalize into an MSISDN.
-  def self.msisdnize(number)
-    number = normalize(number)
-    number = "47#{number}" unless number =~ /^\+/
-    number.gsub!(/^\+/, '')
-    number
+    def msisdnize(number)
+      number = normalize(number)
+      number = "47#{number}" unless number =~ /^\+/
+      number.gsub!(/^\+/, '')
+      number
+    end
   end
 end
