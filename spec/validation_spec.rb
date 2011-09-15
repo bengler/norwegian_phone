ec7a99dd41f0af53ebd031858b15e319baa5696e
Katrina Owen <katrina.owen@gmail.com>
2011-09-15 11:40:18 +0200

    Fix broken tests

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,41 +1,41 @@
 require 'spec_helper'
 
-describe NoPhone, "#valid?" do
+describe NoPhone, "#number_valid?" do
   it "is invalid if nil" do
-    NoPhoneWrapper.valid?(nil).should be_false
+    NoPhoneWrapper.number_valid?(nil).should be_false
   end
 
   it "is invalid if blank" do
-    NoPhoneWrapper.valid?("").should be_false
+    NoPhoneWrapper.number_valid?("").should be_false
   end
 
   it "is invalid with letters" do
-    NoPhoneWrapper.valid?("1abc2").should be_false
+    NoPhoneWrapper.number_valid?("1abc2").should be_false
   end
 
   context "with funky characters" do
     it "rejects parens" do
-      NoPhoneWrapper.valid?("123(45)678").should be_false
+      NoPhoneWrapper.number_valid?("123(45)678").should be_false
     end
 
     it "rejects hyphens" do
-      NoPhoneWrapper.valid?("123-45-678").should be_false
+      NoPhoneWrapper.number_valid?("123-45-678").should be_false
     end
 
     it "rejects periods" do
-      NoPhoneWrapper.valid?("123.45.678").should be_false
+      NoPhoneWrapper.number_valid?("123.45.678").should be_false
     end
 
     it "rejects +sign in the middle" do
-      NoPhoneWrapper.valid?("123+45+678").should be_false
+      NoPhoneWrapper.number_valid?("123+45+678").should be_false
     end
 
     it "accepts spaces" do
-      NoPhoneWrapper.valid?("123 45 678").should be_true
+      NoPhoneWrapper.number_valid?("123 45 678").should be_true
     end
 
     it "accepts +sign in the beginning" do
-      NoPhoneWrapper.valid?("+123 45 678").should be_true
+      NoPhoneWrapper.number_valid?("+123 45 678").should be_true
     end
   end
 end
